55fe6ea9d91070c9739aa0ac21d2521c91a7c3f6
Chrisgrm <chrisgrm@hotmail.com>
2023-03-07 17:20:50 -0500

    Update Mundo.cs

--- My project/Assets/Scripts/Mundo.cs
+++ My project/Assets/Scripts/Mundo.cs
@@ -7,7 +7,7 @@ public class Mundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ///prueba
     }
 
     // Update is called once per frame
